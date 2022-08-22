c5ab0463f76fdc48d4ea0fdb5857d31c638cdae5
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-22 08:52:24 -0700

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,5 @@
+﻿using StaticExercise;
+
+Console.WriteLine($"50 degress Farenheit is {TempConverter.FarenheitToCelsius(50)} degrees Celsius");
+
+Console.WriteLine($"30 degress Celsius is {TempConverter.CelsiusToFarenheit(30)} degrees Farenheit.");
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+            return (farenheit - 32) * 5 / 9;
+        }
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            return (celsius * 1.8) + 32;
+        }
+
+    }
+}
